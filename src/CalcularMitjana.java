152aeed5f73e88b835fe38c340d10c84b2988bf0
ByGamer01 <davidgilrosa1@gmail.com>
2026-02-20 17:08:05 +0100

    a

--- src/CalcularMitjana.java
+++ src/CalcularMitjana.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
@@ -86,23 +85,3 @@ public class CalcularMitjana extends JFrame {
         this.setVisible(true);
     }
 }
-=======
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
-import java.awt.Event;
-import java.awt.event.ActionEvent;
-public class CalcularMitjana extends JPanel implements ActionListener{
-    public CalcularMitjana(){
-        this.setTitle("Calcular Mitjana");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setDefaultLookAndFeelDecorated(true);
-        this.setVisible(true);
-    }
-    @Override
-    public void actionPerformed(ActionEvent e) {
-    }
-}
->>>>>>> 8741f15518e59c4c72fde36a7b82409f893e7f2c
