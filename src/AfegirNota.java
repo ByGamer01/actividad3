d05e1bcfcfe8c9f5032255071744d7aabb024710
ByGamer01 <davidgilrosa1@gmail.com>
2026-02-20 16:08:12 +0100

    x

--- src/AfegirNota.java
+++ src/AfegirNota.java
@@ -16,15 +16,16 @@ public class AfegirNota extends JFrame implements ActionListener {
         this.setBackground(Color.CYAN);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        setDefaultLookAndFeelDecorated(true);
 
         JPanel agregarNotas = new JPanel();
         this.add(agregarNotas);
         agregarNotas.setLayout(new FlowLayout());
 
-
         JLabel lblAsignaturas = new JLabel("Asignatura: ");
-        JTextField txtAsignatura = new JTextField(10);
-        
+        JComboBox<String> txtAsignatura = new JComboBox<>(
+                new String[] { "Matematicas", "Castellano", "Ingles", "Catalan" });
+
         agregarNotas.add(lblAsignaturas);
         agregarNotas.add(txtAsignatura);
 
@@ -32,7 +33,6 @@ public class AfegirNota extends JFrame implements ActionListener {
         JTextField txtNotas = new JTextField(10);
         agregarNotas.add(lblNotas);
         agregarNotas.add(txtNotas);
-
 
         JButton botonAgregarNota = new JButton("Pulsa Aqui"); // Boton donde el usuario si presiona comienza la logica.
         agregarNotas.add(new JLabel());
@@ -45,12 +45,12 @@ public class AfegirNota extends JFrame implements ActionListener {
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent evt) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
     }
 
     public static void main(String[] args) {
-        new AfegirNota();        
+        new AfegirNota();
     }
 }
---
+++ src/org/jvnet/substance/skin/SubstanceOfficeSilver2007LookAndFeel.java
@@ -0,0 +1,5 @@
+package org.jvnet.substance.skin;
+
+public class SubstanceOfficeSilver2007LookAndFeel {
+
+}
